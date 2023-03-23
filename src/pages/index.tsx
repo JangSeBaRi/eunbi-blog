de866c752a65bfc854a878e7310c461b71f96fff
JangSeBaRi <jangsj0325@gmail.com>
2023-03-23 12:10:29 +0900

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,61 @@
 /* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import Image from "next/image";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import HomeIcon from "@mui/icons-material/Home";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import ArticleIcon from "@mui/icons-material/Article";
+import GroupIcon from "@mui/icons-material/Group";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [openMenu, setOpenMenu] = useState<boolean>(true);
     const categoryList = [
         {
             id: "home",
             label: "홈",
+            icon: <HomeIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></HomeIcon>,
         },
         {
             id: "publish",
-            label: "출판사",
+            label: "웅 출판사",
+            icon: <CorporateFareIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></CorporateFareIcon>,
         },
         {
             id: "book",
             label: "책",
+            icon: <AutoStoriesIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></AutoStoriesIcon>,
         },
         {
             id: "daily",
             label: "일간 웅비",
+            icon: <SubscriptionsIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></SubscriptionsIcon>,
         },
         {
             id: "report",
             label: "보도자료",
+            icon: <ArticleIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></ArticleIcon>,
         },
         {
             id: "collaboration",
             label: "웅비X포둥",
+            icon: <GroupIcon fontSize="small" className=" group-hover:text-blue-700 text-stone-700"></GroupIcon>,
         },
     ];
 
     const snsList1 = [
         {
             id: "instagram",
-            icon: <InstagramIcon className="hover:text-pink-600 cursor-pointer"></InstagramIcon>,
+            icon: (
+                <a href="https://www.instagram.com/eun__bitamin/" target="_blank">
+                    <InstagramIcon className="hover:text-pink-600 cursor-pointer"></InstagramIcon>
+                </a>
+            ),
         },
         {
             id: "facebook",
@@ -44,7 +63,11 @@ export default function Home() {
         },
         {
             id: "youtube",
-            icon: <YouTubeIcon className="hover:text-red-600 cursor-pointer"></YouTubeIcon>,
+            icon: (
+                <a href="https://www.youtube.com/@eun__bitamin" target="_blank">
+                    <YouTubeIcon className="hover:text-red-600 cursor-pointer"></YouTubeIcon>
+                </a>
+            ),
         },
     ];
 
@@ -55,13 +78,32 @@ export default function Home() {
         },
         {
             id: "instagram",
-            icon: <InstagramIcon className="hover:text-pink-600 cursor-pointer" fontSize="large"></InstagramIcon>,
+            icon: (
+                <a href="https://www.instagram.com/eun__bitamin/" target="_blank">
+                    <InstagramIcon className="hover:text-pink-600 cursor-pointer" fontSize="large"></InstagramIcon>
+                </a>
+            ),
         },
         {
             id: "youtube",
-            icon: <YouTubeIcon className="hover:text-red-600 cursor-pointer" fontSize="large"></YouTubeIcon>,
+            icon: (
+                <a href="https://www.youtube.com/@eun__bitamin" target="_blank">
+                    <YouTubeIcon className="hover:text-red-600 cursor-pointer" fontSize="large"></YouTubeIcon>
+                </a>
+            ),
         },
     ];
+
+    useEffect(() => {
+        const html = document.querySelector("html")!!;
+        if (openMenu) {
+            html.scrollTo({ top: 0, behavior: "smooth" });
+            html.style.overflow = "hidden";
+        } else {
+            html.style.overflow = "scroll";
+        }
+    }, [openMenu]);
+
     return (
         <>
             <Head>
@@ -73,19 +115,22 @@ export default function Home() {
                     rel="stylesheet"
                 ></link>
             </Head>
-            <div>
-                <nav className="bg-stone-100 flex items-center justify-between px-24">
-                    <ul className="flex items-center">
+            <div className="relative">
+                <nav className="bg-stone-100 flex items-center justify-between h-14">
+                    <ul className="flex items-center h-full pl-24 max-560px:hidden">
                         {categoryList.map((category) => {
                             const { id, label } = category;
                             return (
-                                <li key={id} className="px-3 py-[14px] text-sm cursor-pointer hover:font-bold">
+                                <li
+                                    key={id}
+                                    className="px-3 text-sm h-full flex items-center cursor-pointer hover:font-bold"
+                                >
                                     {label}
                                 </li>
                             );
                         })}
                     </ul>
-                    <div className="flex items-center gap-10">
+                    <div className="flex items-center gap-10 pr-24 max-810px:hidden">
                         <span className="text-sm">eunbi@naver.com</span>
                         <ul className="flex items-center gap-3">
                             {snsList1.map((category) => {
@@ -98,8 +143,29 @@ export default function Home() {
                             })}
                         </ul>
                     </div>
+                    <div className="flex flex-col items-end justify-center w-full h-full 560px:hidden">
+                        <div
+                            className="relative z-[2] flex flex-col justify-between h-[10px] mr-5 cursor-pointer"
+                            onClick={() => {
+                                setOpenMenu(!openMenu);
+                            }}
+                        >
+                            <div
+                                className="w-[20px] h-[2px] bg-stone-800 rounded-full transition-all duration-200 relative top-0"
+                                style={openMenu ? { transform: "rotate(45deg)", top: 4 } : undefined}
+                            />
+                            <div
+                                className="w-[20px] h-[2px] bg-stone-800 rounded-full transition-all duration-200"
+                                style={openMenu ? { opacity: 0 } : undefined}
+                            />
+                            <div
+                                className="w-[20px] h-[2px] bg-stone-800 rounded-full transition-all duration-200 relative top-0"
+                                style={openMenu ? { transform: "rotate(-45deg)", top: -4 } : undefined}
+                            />
+                        </div>
+                    </div>
                 </nav>
-                <main className="flex flex-col items-center">
+                <main className="flex flex-col items-center max-560px:px-8">
                     <img
                         src="https://static.wixstatic.com/media/008770_0409ab0178c9422192d4deb681c97e68~mv2_d_2280_2280_s_2.jpg/v1/fill/w_93,h_93,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/%EB%A1%9C%EA%B3%A0%EC%A0%95%EB%B0%A9%ED%98%95.jpg"
                         alt="logo"
@@ -108,10 +174,12 @@ export default function Home() {
                     <span className="font-Londrina-Outline text-2xl mt-4">Eunbi.com</span>
                     <span className="mt-2">조은비</span>
                     <div className="relative mt-[72px]">
-                        <img
-                            src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogpfthumb.phinf.naver.net%2FMjAyMjAxMjhfNTgg%2FMDAxNjQzMzQyMTkyMTky.iui0JRvhb7MC5LICOEb_tEnq4XOtIvCuZlbx-jTz9-Qg.hBGCXFy5MorZZKgKV7o387uE3dTPuT2RFa4iaWrkTx0g.PNG.bora402319%2FprofileImage.png"
-                            alt="profile"
-                            className=" rounded-full w-72 h-72"
+                        <Image
+                            src={"/eunbi/3.jpeg"}
+                            width={288}
+                            height={288}
+                            alt="eunbi_picture"
+                            className="mx-3 rounded-full max-w-72 max-h-72"
                         />
                         <span className="absolute right-0 bottom-0 text-xs opacity-80 font-serif">
                             <span className="opacity-50">©</span> 은비
@@ -119,7 +187,6 @@ export default function Home() {
                     </div>
                     <div className="mt-10 text-lg font-serif flex flex-col items-center leading-6 font-bold">
                         <span className=" text-xl">조은비</span>
-                        <span className=" tracking-[-1px]">張世眞</span>
                     </div>
                     <div className="flex flex-col items-center gap-10 font-korean text-sm">
                         <div />
@@ -135,50 +202,58 @@ export default function Home() {
                             <p>2018</p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>아무렇게나 춤춰 아무렇지 않아 보이게
-                                아무 생각 하기 싫어 아무개로 살래 잠시 im sick and tired⌟
+                                아무 생각 하기 싫어 아무개로 살래 잠시 im sick and tired
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>of my every day, keep it up 한 곡 더
-                                아무 노래나 일단 틀어 아무렴 어때 its so boring 아무래도⌟
+                                아무 노래나 일단 틀어 아무렴 어때 its so boring 아무래도
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                         </div>
                         <div className="flex flex-col gap-[10px] text-center">
                             <p>2019</p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>refresh가 시급한 듯해 쌓여가 스트레스가
-                                배꼽 빠질 만큼만 폭소하고 싶은 날이야 What up my dawgs?⌟
+                                배꼽 빠질 만큼만 폭소하고 싶은 날이야 What up my dawgs?
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>어디야 너희 올 때 병맥주랑 까까 몇 개 사
-                                와 uh 클럽은 구미가 잘 안 당겨 우리 집 거실로 빨랑 모여⌟
+                                와 uh 클럽은 구미가 잘 안 당겨 우리 집 거실로 빨랑 모여
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>외부인은 요령껏 차단 시켜 밤새 수다 떨
-                                시간도 모자라 누군 힘들어 죽겠고 누군 축제 괜히 싱숭생숭⌟
+                                시간도 모자라 누군 힘들어 죽겠고 누군 축제 괜히 싱숭생숭
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                         </div>
                         <div className="flex flex-col gap-[10px] text-center">
                             <p>2020</p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>I want my youth back 좀 전까지 왁자지껄
-                                하다 한 명 두 명씩 자릴 떠 왜들 그리 다운돼있어? 뭐가⌟
+                                하다 한 명 두 명씩 자릴 떠 왜들 그리 다운돼있어? 뭐가
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                         </div>
                         <div className="flex flex-col gap-[10px] text-center">
                             <p>2021</p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>문제야 say something 분위기가 겁나 싸해
-                                요새는 이런 게 유행인가 왜들 그리 재미없어? 아 그건 나도⌟
+                                요새는 이런 게 유행인가 왜들 그리 재미없어? 아 그건 나도
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>마찬가지 Tell me what I got to do 급한
-                                대로 블루투스 켜⌟
+                                대로 블루투스 켜<span className="relative top-[2px]">⌟</span>
                             </p>
                         </div>
                         <div className="flex flex-col gap-[10px] text-center">
                             <p>2022</p>
                             <p>
                                 <span className="relative -top-[3.5px]">⌜</span>아무 노래
+                                <span className="relative top-[2px]">⌟</span>
                             </p>
                         </div>
                     </div>
@@ -196,6 +271,44 @@ export default function Home() {
                     </span>
                     <span>eunbi@naver.com</span>
                 </footer>
+                {openMenu && (
+                    <div
+                        className="absolute top-0 left-0 w-screen h-full bg-stone-800/[0.5] animate-fadein"
+                        onClick={() => {
+                            setOpenMenu(false);
+                        }}
+                    />
+                )}
+                <section
+                    className="absolute top-0 z-[1] bg-white h-full transition-['right']"
+                    style={{ width: "calc(100% - 40px)", right: openMenu ? 0 : "calc(-100% + 40px)" }}
+                >
+                    <header className="flex items-center h-14 border-b-[1px] border-b-stone-300">
+                        <Image
+                            src={"/eunbi/1.jpeg"}
+                            width={40}
+                            height={40}
+                            alt="eunbi_picture"
+                            className="mx-4 rounded-full"
+                        />
+                        <span className="text-sm">eun__bitamin blog</span>
+                    </header>
+                    <main>
+                        <ul className="flex flex-col gap-3 pt-3">
+                            {categoryList.map((category) => {
+                                const { id, label, icon } = category;
+                                return (
+                                    <li key={id} className="pr-2">
+                                        <button className="flex items-center py-3 pl-7 w-full rounded-r-full hover:bg-blue-50 group">
+                                            {icon}
+                                            <span className="ml-6 text-sm text-stone-700 font-semibold group-hover:text-blue-700">{label}</span>
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </main>
+                </section>
             </div>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { keyframes } = require('@emotion/react');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,19 @@ module.exports = {
     extend: {
       fontSize: {
         "text-xs" : "0.75rem"
+      },
+      screens: {
+        "560px" : "560px",
+        "810px" : "810px"
+      },
+      animation: {
+        fadein: 'fadein 0.2s ease-in-out'
+      },
+      keyframes: {
+        fadein: {
+          "0%" : {opacity: 0},
+          "100%" : {opacity: 100},
+        }
       }
     },
   },
